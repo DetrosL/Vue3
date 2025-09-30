45231fb3be66529159243ed664b158a5e8688675
Laiz <megami.ay@gmail.com>
2025-09-30 15:51:42 -0300

    fix: places

---
+++ codes/with-buildtools/00ex/src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Header
+    </div>
+</template>
+<script>
+    export default {
+        name: 'App',
+        components: {
+        HelloWorld
+        }
+    }
+</script>
+<style>
+    
+</style>
