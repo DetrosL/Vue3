f2fed06a26763239818200fda898857a2c9b4ec5
Laiz <megami.ay@gmail.com>
2025-10-01 09:02:30 -0300

    feat: loops

--- codes/with-buildtools/00ex/src/App.vue
+++ codes/with-buildtools/00ex/src/App.vue
@@ -2,36 +2,24 @@
   <TheHeader 
     v-show="showHeader"
   /><!-- ou <TheHeader></TheHeader> -->
-
-  <div v-show="Name">
-    Nome: {{ fistName }} <br/>
-    Nome: {{ lastName }}
-  </div>
   
   <div v-show="showName">
-    Nome: {{ fistName }} <br/>
-    Nome: {{ lastName }}
+    Name: {{ fistName }} <br/>
+    Last Name: {{ lastName }}
   </div>
 
   <div v-if="accessLevel === 'admin'">Admin</div>
   <div v-else-if="accessLevel === 'marketing'">Marketing</div>
   <div v-else>User</div>
 
-  <img
-    alt="Vue logo" 
-    src="./assets/logo.png"
-    >
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld.vue'
   import TheHeader from './components/TheHeader.vue'
 
   export default {
     name: 'App',
     components: {
-      HelloWorld,
       TheHeader
     },
     data(){
@@ -47,12 +35,5 @@
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+  
 </style>
