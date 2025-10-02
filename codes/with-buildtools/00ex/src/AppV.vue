789924c598647838b83b57e1598196efb91e05de
Laiz Detros <megami.ay@gmail.com>
2025-10-02 20:23:35 -0300

    Create AppV.vue

---
+++ codes/with-buildtools/00ex/src/AppV.vue
@@ -0,0 +1,11 @@
+<template>
+  <TheHeader v-show="showHeader" />
+</template>
+<script setup>
+    import { ref } from 'vue'
+    import TheHeader from './components/TheHeaderV.vue'
+
+    const showHeader = ref(true)
+</script>
+<style>
+</style>
