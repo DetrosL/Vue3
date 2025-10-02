716420e22413a4508b115a9ff493d9c723d71b9f
Laiz Detros <megami.ay@gmail.com>
2025-10-02 20:18:07 -0300

    Create AppV.vue

---
+++ codes/with-buildtools/00ex/src/AppV.vue
@@ -0,0 +1,24 @@
+<template>
+  <TheHeader v-show="showHeader" />
+
+  <div v-show="showName">
+    Name: {{ firstName }} <br/>
+    Last Name: {{ lastName }}
+  </div>
+
+  <div v-if="accessLevel === 'admin'">Admin</div>
+  <div v-else-if="accessLevel === 'marketing'">Marketing</div>
+  <div v-else>User</div>
+</template>
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+import { ref } from 'vue'
+
+const showHeader = ref(true)
+const firstName = ref('Laiz')
+const lastName = ref('Detros')
+const showName = ref(false)
+const accessLevel = ref('admin')
+</script>
+<style>
+</style>
